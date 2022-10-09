638d382d9118dcba82580a65ab8a8c001bd346c0
LeanoraVilte <tina.ww.lea@gmail.com>
2022-10-09 22:10:34 +0300

    Innopolis6677666

--- src/Converter.java
+++ src/Converter.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Converter {public static void main(String[] ar
-    gs) {
+public class Converter {public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         while (true) {
             String st = s.nextLine();
--- src/test4.java
+++ src/test4.java
@@ -1,7 +1,10 @@
-
+import java.util.Scanner;
 
 public class test4 {
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
     }
 }
